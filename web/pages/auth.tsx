d6bd74760ed1d62c6918af67a86db1c94490c59d
Dev-Siri <s0431668@gmail.com>
2022-11-26 22:37:23 +0530

    fix auth profile picture input

--- web/pages/auth.tsx
+++ web/pages/auth.tsx
@@ -79,6 +79,7 @@ const Auth: NextPage = () => {
           />
           {isSignup && (
             <input
+              type="file"
               aria-label="Profile Picture Upload"
               className="mt-5 ml-[5px] w-full rounded-md border-[1px] border-light-gray p-[15px] outline-none duration-[250ms] focus:border-primary dark:border-semi-gray dark:bg-semi-gray dark:text-white"
               onChange={event => setFormData({ ...formData, photoURL: event.target.files?.[0] })}
