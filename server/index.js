0489bdbd2f52c06fca7cd386dea9930033f3a291
Dev-Siri <s0431668@gmail.com>
2022-11-27 01:40:13 +0530

    Server & Web communication made secure and functional + Deployed server to railway

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(
   cors({
-    origin: process.env.NODE_PRIVATE_FRONTEND_WEB_URL,
+    origin: process.env.NODE_ENV === "development" ? "http://localhost:3000" : process.env.NODE_PRIVATE_FRONTEND_WEB_URL,
     methods: ["GET", "POST", "PATCH"],
   })
 );
@@ -26,9 +26,11 @@ app.get("/", (_, res) => res.status(200).send());
 app.use("/posts", postRoutes);
 app.use("/users", userRoutes);
 
-const CONNECTION_URL = `${process.env.NODE_PRIVATE_MONGO_DB_CONNECTION_URL}`;
-const PORT = `${process.env.PORT}` || 5000;
+const CONNECTION_URL = process.env.NODE_PRIVATE_MONGO_DB_CONNECTION_URL;
+const PORT = process.env.PORT || 5000;
 
 await connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.listen(PORT, () => console.log(`Server running on Port: ${PORT}`));
+
+// https://tea-share-backend.onrender.com/users
