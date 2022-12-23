bd8366f83de74e6b13e302c001075cf77a6d84d1
Dev-Siri <s0431668@gmail.com>
2022-12-23 22:56:03 +0530

    Fix smaller banner not showing

--- web/constants/pageInfo.ts
+++ web/constants/pageInfo.ts
@@ -4,5 +4,5 @@ export const PAGE_DESCRIPTION: string = "Tea share is a brand new online social 
 export const PAGE_URL: string = "https://tea-share.vercel.app";
 export const PAGE_TITLE: string = "Tea Share - A Brand New Social Networking Platform";
 export const PAGE_SHORT_TITLE: string = "Tea Share";
-export const PAGE_FAVICON_PATH: string = "/favicon.png";
+export const PAGE_FAVICON_PATH: string = "/favicon.ico";
 export const PAGE_KEYWORDS = "Tea share, Tea, Social Media, Social Networking, News, Connect";
