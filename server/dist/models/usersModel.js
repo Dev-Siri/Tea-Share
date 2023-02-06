05f2f9d2ab49f1f85540b861bf9bd11251e0e75d
Dev-Siri <s0431668@gmail.com>
2023-02-07 01:48:23 +0530

    remove dist

--- server/dist/models/usersModel.js
+++
@@ -1,8 +0,0 @@
-import { Schema, model } from "mongoose";
-const UserSchema = new Schema({
-    username: String,
-    email: String,
-    image: String
-});
-const UserModel = model("registeredUsers", UserSchema);
-export default UserModel;
--- server/dist/routes/user.js
+++
@@ -1,8 +0,0 @@
-import { createUser, getUserBySearchTerm, getUsers, updateUser } from "../controllers/users.js";
-const userRoutes = async (fastify) => {
-    fastify.get("/users", getUsers);
-    fastify.post("/users", createUser);
-    fastify.patch("/users/:id", updateUser);
-    fastify.get("/users/search", getUserBySearchTerm);
-};
-export default userRoutes;
