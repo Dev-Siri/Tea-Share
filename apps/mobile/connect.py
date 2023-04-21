9ecb6097850a883f5f075eb983d04575b4815ab4
Dev-Siri <s0431668@gmail.com>
2023-04-22 02:36:48 +0530

    Update connect.py

--- apps/mobile/connect.py
+++ apps/mobile/connect.py
@@ -2,10 +2,10 @@
 
 import argparse
 import subprocess
+from threading import Thread
 
 def connect_to_phone(ip: str, port: int):
   connection_ip: str = f"{ip}:{port}"
-
   subprocess.run(["adb", "kill-server"])
   subprocess.run(["adb", "tcpip", str(port)])
   subprocess.run(["adb", "connect", connection_ip])
@@ -15,8 +15,10 @@ def main():
   parser.add_argument("ip", type=str, help="the IP address of the phone")
   parser.add_argument("--port", type=int, default=5555, help="the port number to use (default: 5555)")
   args = parser.parse_args()
- 
-  connect_to_phone(args.ip, args.port)
+
+  t = Thread(target=connect_to_phone, args=(args.ip, args.port))
+  t.start()
+  t.join()
 
 if __name__ == "__main__":
   main()
