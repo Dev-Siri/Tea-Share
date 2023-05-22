ac58d375ea6361b510e966ca58b6e7f14d2a06f8
Dev-Siri <s0431668@gmail.com>
2023-05-22 18:51:39 +0530

    Add mising  property from posts model in the new server

--- apps/server/models/post.go
+++ apps/server/models/post.go
@@ -10,6 +10,7 @@ type Post struct {
 	ID          primitive.ObjectID `json:"_id" bson:"_id,omitempty"`
 	Title       string             `json:"title" bson:"title"`
 	Description string             `json:"description" bson:"description"`
+	Image       string             `json:"image" bson:"image"`
 	Author      string             `json:"author" bson:"author"`
 	AuthorImage string             `json:"authorImage" bson:"authorImage"`
 	CreatedAt   time.Time          `json:"createdAt" bson:"createdAt"`
