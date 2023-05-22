595ecf1f2a659e08deeee119fb627b9dcf2c8270
Dev-Siri <s0431668@gmail.com>
2023-05-22 11:37:19 +0530

    Remove the remnant of the old (main)/create/page.tsx comment

--- apps/web/app/(main)/create/page.tsx
+++ apps/web/app/(main)/create/page.tsx
@@ -9,50 +9,30 @@ import LoadingSpinner from "@/components/LoadingSpinner";
 const SubmitButton = lazy(() => import("@/components/SubmitButton"));
 const PreviewImage = lazy(() => import("@/components/PreviewImage"));
 
-const Create: PageComponent = () => {
-  // const handleCreate: FormEventHandler<HTMLFormElement> = async event => {
-  //   event.preventDefault();
-
-  //   const form = new FormData(event.currentTarget);
-  //   const formData = Object.fromEntries(form.entries()) as Pick<PostFormData, "title" | "description" | "image">;
-
-  //   if (!(formData.image as File).type.includes("image")) {
-  //     const { toast } = await import("react-hot-toast");
-  //     return toast.error("The uploaded file is not an image.");
-  //   }
-
-  //   const { CreatePost } = await import("@/utils/posts");
-
-  //   await CreatePost(formData);
-
-  //   router.push("/");
-  // };
-
-  return (
-    <section className="h-screen w-full overflow-y-scroll">
-      <form action={createPost} className="flex w-full flex-col items-center py-2">
-        <input
-          className="dark:bg-semi-gray w-[90%] rounded-tl-lg rounded-tr-lg p-4 text-4xl font-light outline-none duration-200 dark:text-white"
-          placeholder="Title"
-          name="title"
-          required
-        />
-        <input
-          className="dark:bg-semi-gray mb-4 w-[90%] rounded-bl-lg rounded-br-lg p-4 pl-5 font-normal outline-none duration-200 dark:text-white"
-          placeholder="What's on your mind?"
-          name="description"
-          required
-        />
-        <PreviewImage />
-        <SubmitButton
-          loadingSpinner={<LoadingSpinner height={20} width={20} />}
-          className="bg-primary hover:bg-primary-dark mb-[30%] mt-4 w-[90%] cursor-pointer rounded-md border-none p-3 text-white duration-200 sm:mb-[10%]"
-        >
-          Create Post!
-        </SubmitButton>
-      </form>
-    </section>
-  );
-};
+const Create: PageComponent = () => (
+  <section className="h-screen w-full overflow-y-scroll">
+    <form action={createPost} className="flex w-full flex-col items-center py-2">
+      <input
+        className="dark:bg-semi-gray w-[90%] rounded-tl-lg rounded-tr-lg p-4 text-4xl font-light outline-none duration-200 dark:text-white"
+        placeholder="Title"
+        name="title"
+        required
+      />
+      <input
+        className="dark:bg-semi-gray mb-4 w-[90%] rounded-bl-lg rounded-br-lg p-4 pl-5 font-normal outline-none duration-200 dark:text-white"
+        placeholder="What's on your mind?"
+        name="description"
+        required
+      />
+      <PreviewImage />
+      <SubmitButton
+        loadingSpinner={<LoadingSpinner height={20} width={20} />}
+        className="bg-primary hover:bg-primary-dark mb-[30%] mt-4 w-[90%] cursor-pointer rounded-md border-none p-3 text-white duration-200 sm:mb-[10%]"
+      >
+        Create Post!
+      </SubmitButton>
+    </form>
+  </section>
+);
 
 export default Create;
