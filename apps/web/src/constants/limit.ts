a10e0617c3d01bf2440165a98a1437006917ee3c
Dev-Siri <s0431668@gmail.com>
2023-06-03 15:49:47 +0530

    Performance improvements, bug fixes && next.js update to 13.4.5-canary.4

--- apps/web/src/constants/limit.ts
+++
@@ -1,4 +0,0 @@
-export const POST_LIMIT = 8;
-export const USER_LIMIT = 10;
-
-export const INITIAL_PAGE_LIMIT = 1;
--- apps/web/src/utils/globals.ts
+++ apps/web/src/utils/globals.ts
@@ -1,16 +1,17 @@
-import { TIME_UNITS, TIME_UNIT_SECONDS } from "@/constants/date";
-
 export const getRelativeTime = (dateString: string) => {
   const date = new Date(dateString);
   const timeMs = date.getTime();
   const deltaSeconds = Math.round((timeMs - Date.now()) / 1000);
-  const unitIndex = TIME_UNIT_SECONDS.findIndex(timeUnitSecond => timeUnitSecond > Math.abs(deltaSeconds));
+  const timeUnitSeconds = [60, 3600, 86400, 604800, 2592000, 31536000, Infinity];
+  const timeUnits = ["second", "minute", "hour", "day", "week", "month", "year"] as const;
 
-  const divisor = unitIndex ? TIME_UNIT_SECONDS[unitIndex - 1] : 1;
+  const unitIndex = timeUnitSeconds.findIndex(timeUnitSecond => timeUnitSecond > Math.abs(deltaSeconds));
+
+  const divisor = unitIndex ? timeUnitSeconds[unitIndex - 1] : 1;
   const rtf = new Intl.RelativeTimeFormat("en", { numeric: "auto" });
 
   try {
-    return rtf.format(Math.floor(deltaSeconds / divisor), TIME_UNITS[unitIndex]);
+    return rtf.format(Math.floor(deltaSeconds / divisor), timeUnits[unitIndex]);
   } catch (error) {
     console.error(error);
     return "Error parsing date";
