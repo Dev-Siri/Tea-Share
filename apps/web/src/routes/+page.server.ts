99aa92256929a661f160db387dd07553cb61bb57
Dev-Siri <s0431668@gmail.com>
2023-06-11 22:07:52 +0530

    Run the home page of web on the edge

--- apps/web/src/routes/+page.server.ts
+++ apps/web/src/routes/+page.server.ts
@@ -1,6 +1,11 @@
 import queryClient from "../services/queryClient";
 
+import type { Config } from "@sveltejs/adapter-vercel";
 import type { MongoDBUser, Post } from "../app";
+
+export const config: Config = {
+  runtime: "edge",
+};
 
 export const load = async () => {
   const [posts, users] = await Promise.all([
