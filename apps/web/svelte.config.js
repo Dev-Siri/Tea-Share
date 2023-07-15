855bc68f8dd30f6dbce7e769a93e2e95c00d0bc7
Dev-Siri <s0431668@gmail.com>
2023-07-16 01:13:26 +0530

    Change maxDuration -> 10 for vercel hobby plan

--- apps/web/svelte.config.js
+++ apps/web/svelte.config.js
@@ -5,7 +5,7 @@ import { vitePreprocess } from "@sveltejs/kit/vite";
 const config = {
   preprocess: [vitePreprocess()],
   kit: {
-    adapter: adapter({ maxDuration: 20 }),
+    adapter: adapter({ maxDuration: 10 }),
   },
 };
 
